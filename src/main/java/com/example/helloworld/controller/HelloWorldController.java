f1368dd1fe4a624cf8b0c55bdc112381e1cbcdb6
Shubhayu Chongder <62900893+chongdershubhayu@users.noreply.github.com>
2023-11-08 20:46:03 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,11 +1,15 @@
 package com.example.helloworld.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${greeter.message}")
+    private String greeterMessageFormat;
+    
     @GetMapping("/hello")
     public String sendGreetings() {
         return "Hello, World!";
